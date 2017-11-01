0fbb74e9c423ee59757192e9c115a3f2b45427f9
zkeyword <zjx1247225@163.com>
2017-11-01 14:15:41 +0800

    引入antd ng版

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule, ReactiveFormsModule } from "@angular/forms"
+import { NgZorroAntdModule } from 'ng-zorro-antd'
+
+import { LoginRoutingModule } from './login-routing.module'
 
 import { LoginComponent } from '../../components/login/login.component'
-import { LoginRoutingModule } from './login-routing.module'
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
-        LoginRoutingModule
+        LoginRoutingModule,
+        NgZorroAntdModule
     ],
     declarations: [
         LoginComponent
--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgZorroAntdModule } from 'ng-zorro-antd'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgZorroAntdModule
   ],
   declarations: []
 })
